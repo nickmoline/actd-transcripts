1c14f4c8c10d5454bf1c49beb8d8f4c363dd0645
Nick Moline <nick.moline@justia.com>
2014-08-12 20:43:31 -0700

    Bunch of feature enhancements and code separation

---
+++ include.php
@@ -0,0 +1,165 @@
+<?php
+
+/**
+ * Check an array key to make sure it exists (and is optionally numeric value) without triggering PHP Notices
+ *
+ * @author  nickmoline
+ * @since  2013-03-12
+ * @version  2013-08-23
+ * @param  string  $key     array key to check for
+ * @param  array   $array   array to check for the existance of $key in
+ * @param  boolean $numeric if true value must be numeric to return
+ * @return mixed $array[$key] if it exists (and passes $numeric check) or null if not
+ */
+function jcheck_array_val($key, $array = array(), $numeric = false) {
+	if (!is_array($array)) return null;
+	if (!isset($array[$key])) return null;
+	if ($numeric) {
+		if (is_numeric($array[$key])) return $array[$key];
+	} else {
+		return $array[$key];
+	}
+	return null;
+}
+
+/**
+ * Check $_REQUEST for a given key and return it if it exists (and optionally passes numeric test)
+ *
+ * @author  nickmoline
+ * @since 2013-03-12
+ * @version  2013-03-12
+ * @param  string  $key     array key to check for
+ * @param  boolean $numeric if true value must be numeric to return
+ * @return mixed $_REQUEST[$key] if it exists (and passes $numeric check) or null if not
+ */
+function jcheck_request($key, $numeric = false) {
+	return jcheck_array_val($key, $_REQUEST, $numeric);
+}
+
+/**
+ * Check $_SERVER for a given key and return it if it exists (and optionally passes numeric test)
+ *
+ * @author  nickmoline
+ * @since 2013-05-08
+ * @version  2013-05-08
+ * @param  string  $key     array key to check for
+ * @param  boolean $numeric if true value must be numeric to return
+ * @return mixed $_SERVER[$key] if it exists (and passes $numeric check) or null if not
+ */
+function jcheck_server($key, $numeric = false) {
+	return jcheck_array_val($key, $_SERVER, $numeric);
+}
+
+/**
+ * Check $_GET for a given key and return it if it exists (and optionally passes numeric test)
+ *
+ * @author  nickmoline
+ * @since 2013-03-12
+ * @version  2013-03-12
+ * @param  string  $key     array key to check for
+ * @param  boolean $numeric if true value must be numeric to return
+ * @return mixed $_GET[$key] if it exists (and passes $numeric check) or null if not
+ */
+function jcheck_get($key, $numeric = false) {
+	return jcheck_array_val($key, $_GET, $numeric);
+}
+
+/**
+ * Check $_POST for a given key and return it if it exists (and optionally passes numeric test)
+ *
+ * @author  nickmoline
+ * @since 2013-03-12
+ * @version  2013-03-12
+ * @param  string  $key     array key to check for
+ * @param  boolean $numeric if true value must be numeric to return
+ * @return mixed $_POST[$key] if it exists (and passes $numeric check) or null if not
+ */
+function jcheck_post($key, $numeric = false) {
+	return jcheck_array_val($key, $_POST, $numeric);
+}
+
+/**
+ * Check $_COOKIE for a given key and return it if it exists (and optionally passes numeric test)
+ *
+ * @author  nickmoline
+ * @since 2013-03-12
+ * @version  2013-03-12
+ * @param  string  $key     array key to check for
+ * @param  boolean $numeric if true value must be numeric to return
+ * @return mixed $_COOKIE[$key] if it exists (and passes $numeric check) or null if not
+ */
+function jcheck_cookie($key, $numeric = false) {
+	return jcheck_array_val($key, $_COOKIE, $numeric);
+}
+
+/**
+ * Checks if the current user is using SSL to access the site (either directly or via a load balancer)
+ *
+ * @author  nickmoline
+ * @since  2013-09-06
+ * @version  2013-09-06
+ * @return  bool true if SSL, false if not
+ */
+function jcheck_ssl() {
+	$https = jcheck_server('HTTPS');
+	if ($https && strtolower($https) != 'off') return true;
+
+	$forwarded_proto = jcheck_server('HTTP_X_FORWARDED_PROTO');
+	if ($forwarded_proto && strtolower($forwarded_proto) == 'https') return true;
+
+	$forwarded_ssl = jcheck_server('HTTP_X_FORWARDED_SSL');
+	if ($forwarded_ssl && strtolower($forwarded_ssl) == 'on') return true;
+
+	return false;
+}
+
+function jcommon_friendlyize($string, $delim = '-') {
+	$string = jcommon_transliterate_string($string);
+	return trim(preg_replace('@[^A-Za-z0-9]+@msi',$delim,strtolower(trim($string))),$delim);
+}
+
+function jcommon_transliterate_string($string) {
+	$string = html_entity_decode($string, ENT_COMPAT, 'UTF-8');
+	setlocale(LC_ALL, "en_US.UTF-8");
+	return iconv('UTF-8', 'ASCII//TRANSLIT', $string);
+}
+
+function jcommon_normalize_string($string) {
+    $string = html_entity_decode($string,ENT_COMPAT,'UTF-8');
+    return mb_convert_encoding($string, 'HTML-ENTITIES', 'auto');
+}
+
+function get_transcript_list($folder, $default_date_raw = null) {
+    if (!$default_date_raw) $default_date_raw = jcheck_get('date');
+
+    $transcript_list = array();
+
+    $default_date = date("Y-m-d",strtotime($default_date_raw));
+
+    if (is_dir($folder)) {
+		if ($dh = opendir($folder)) {
+			while (($file = readdir($dh)) !== false) {
+				if (preg_match("@#ST_StationMissions_(\d{4})(\d{2})(\d{2}).log@msi", $file, $file_match)) {
+					$file_date_string = "{$file_match[1]}-{$file_match[2]}-{$file_match[3]}";
+					$ts = strtotime($file_date_string);
+					$sd_year = date("y",$ts) + 100;
+					$transcript = array(
+						"stardate"	=>	"{$sd_year}".date("m.d",$ts),
+						"date"		=>	$file_date_string,
+						"selected"	=>	($file_date_string == $default_date)?' selected="selected" class="selected_transcript"':'',
+						"filename"	=>	$file,
+						"full_path"	=>	"{$folder}{$file}",
+					);
+					$transcript_list[$file_date_string] = $transcript;
+				}
+			}
+		}
+	}
+	krsort($transcript_list);
+	if (!$default_date) {
+		$most_recent = current($transcript_list);
+		$default_date = $most_recent['date'];
+		$transcript_list[$default_date]['selected'] = ' selected="selected"';
+	}
+	return $transcript_list;
+}
--- www/index.php
+++ www/index.php
@@ -1,65 +1,64 @@
 <?php
 setlocale(LC_ALL, "en_US.UTF-8");
+mb_http_output( "UTF-8" );
+ob_start("mb_output_handler");
+header('Content-type: text/html; charset=utf-8'); 
 $base_folder = dirname(dirname(__FILE__));
 require_once("{$base_folder}/vendor/autoload.php");
-
+require_once("{$base_folder}/include.php");
 Twig_Autoloader::register();
 
 $twig_loader = new Twig_Loader_Filesystem("{$base_folder}/templates/");
 $twig = new Twig_Environment($twig_loader,
-    array(
-        "autoescape"    =>  false,
-        "cache"         =>  "{$base_folder}/templates/cache/"
-    )
+	array(
+		"autoescape"    =>  false,
+		//"cache"         =>  "{$base_folder}/templates/cache/"
+	)
 );
 
-
-$date_ts = strtotime("2014-08-04");
-
-$date_display = date("y",$date_ts) + 100;
-$date_display .= date("m.d", $date_ts);
-
-$file_date_string = date("Ymd",$date_ts);
-
 $infolder = "{$base_folder}/log/";
-$outfolder = "{$base_folder}/output/";
+$raw_transcript_date = jcheck_get('date');
 
+$transcript_list = get_transcript_list($infolder, $raw_transcript_date);
 
-$base_in_filename = "#ST_StationMissions_";
-
-function jcommon_transliterate_string($string) {
-	$string = html_entity_decode($string, ENT_COMPAT, 'UTF-8');
-	setlocale(LC_ALL, "en_US.UTF-8");
-	return iconv('UTF-8', 'ASCII//TRANSLIT', $string);
+if (!$raw_transcript_date) {
+	$first_entry = current($transcript_list);
+	$raw_transcript_date = $first_entry['date'];
 }
 
+$transcript_date = date("Y-m-d", strtotime($raw_transcript_date));
 
-$in_file = "{$infolder}{$base_in_filename}{$file_date_string}.log";
+$transcript_info = $transcript_list[$transcript_date];
+
+$date_ts = strtotime($transcript_date);
+
+$in_file = $transcript_info['full_path'];
+$date_display = $transcript_info['stardate'];
 
 $positions = array(
-    "co"        =>  "command",
-    "xo"        =>  "command",
-    "fco"       =>  "command",
-    "regent"    =>  "command",
-    "adm"       =>  "command",
-    "capt"      =>  "command",
-    
-    "cto"       =>  "tactical",
-    "to"        =>  "tactical",
-    "csec"      =>  "tactical",
-    
-    "ceo"       =>  "engineering",
-    "eo"        =>  "engineering",
-    "ops"       =>  "engineering",
-    
-    "cmo"       =>  "medical",
-    "mo"        =>  "medical",
-    "cns"       =>  "medical",
-    
-    "cso"       =>  "science",
-    "so"        =>  "science",
-    
-    "civ"       =>  "civilian",
+	"co"        =>  "command",
+	"xo"        =>  "command",
+	"fco"       =>  "command",
+	"regent"    =>  "command",
+	"adm"       =>  "command",
+	"capt"      =>  "command",
+	
+	"cto"       =>  "tactical",
+	"to"        =>  "tactical",
+	"csec"      =>  "tactical",
+	
+	"ceo"       =>  "engineering",
+	"eo"        =>  "engineering",
+	"ops"       =>  "engineering",
+	
+	"cmo"       =>  "medical",
+	"mo"        =>  "medical",
+	"cns"       =>  "medical",
+	
+	"cso"       =>  "science",
+	"so"        =>  "science",
+	
+	"civ"       =>  "civilian",
 );
 
 //$output .=  $in_file;
@@ -78,102 +77,127 @@ $episode_number = "";
 $episode_title = "";
 
 foreach ($raw_lines as $key => $line) {
-    // [20:10:27] <CIV_Nyira> ::heads to her office to send a message to the CO letting him know she is available when ever he needs:: 
-    // [19:40:42] <Pira> <landing pad operator> *FCO*: Sorry Ensign, the pilot seems to have had a bit too much to drink at Nyira's, he's passed out on the floor of his shuttle.
+	// [20:10:27] <CIV_Nyira> ::heads to her office to send a message to the CO letting him know she is available when ever he needs:: 
+	// [19:40:42] <Pira> <landing pad operator> *FCO*: Sorry Ensign, the pilot seems to have had a bit too much to drink at Nyira's, he's passed out on the floor of his shuttle.
 
-    if (preg_match("@^\[(\d+:\d+:\d+)\]\s*<([^>]+)>\s*(.*)$@msi", trim($line), $matches)) {
-        //$output .=  "Found {$key} {$line}\n\n";
-        //print_r($matches);
-        
-        $timestamp      = $matches[1];
-        $who            = $matches[2];
-        $raw_message    = $matches[3];
-        
-        $position = 'normal';
-        
-        // CO_Capt_Bodine
-        /*\
-            array(
-                [0] => "CO",
-                [1] => "Capt",
-                [2] => "Bodine
-            )
-        */
-        $name_parts = explode("_", $who);
-        
-        $position_key = strtolower($name_parts[0]);
-        if (array_key_exists($position_key, $positions)) $position = $positions[$position_key];
-        
-        $formatted_name = implode(" ", $name_parts);
+	if (preg_match("@^\[(\d+:\d+:\d+)\]\s*<([^>]+)>\s*(.*)$@msi", trim($line), $matches)) {
+		//$output .=  "Found {$key} {$line}\n\n";
+		//print_r($matches);
+		
+		$timestamp      = $matches[1];
+		$who            = $matches[2];
+		$raw_message    = $matches[3];
+		
+		$position = 'normal';
+		
+		$name_parts = explode("_", $who);
+		
+		$position_key = strtolower($name_parts[0]);
+		if (array_key_exists($position_key, $positions)) $position = $positions[$position_key];
+		
+		$formatted_name = implode(" ", $name_parts);
 
+		$normalized_message = jcommon_normalize_string($raw_message);
 
-        $encoded_message    = htmlentities($raw_message);
-        
-        if ($last_who && strtolower($who) != $last_who) $output .= "</div>\n\n";
-        if (strtolower($who) != $last_who) {
-            $output .=  "<div id=\"line_{$key}\" class=\"chatline {$who}\">\n";
-            $output .=  "\t<div id=\"line_{$key}_who\" class=\"who {$position} {$who}\">{$formatted_name} says:</div>\n";
-            $last_who = strtolower($who);
-            if (!isset($attendance[$last_who])) {
-                $attendance[$last_who] = array(
-                    "name"          =>  $who,
-                    "display_name"  =>  $formatted_name,
-                    "position"      =>  $position,
-                    "lines"         =>  0,
-                );
-            }
-        }
+		$encoded_message    = str_replace(array('<','>'),array('&lt;','&gt;'), $normalized_message);
+		
+		if (preg_match('@^<([A-Za-z0-9\-\_\s]+)>[:\s]*(.+)$@msi', trim($raw_message), $npc_match)) {
+			$npc_who = "npc_".jcommon_friendlyize($npc_match[1],"_").'_'.jcommon_friendlyize($who,'_');
+			$npc_name_parts = preg_split('@[^A-Za-z0-9\']+@msi', $npc_match[1]);
+			
+			$position = 'normal';
+			$position_key = strtolower($npc_name_parts[0]);
+			if (array_key_exists($position_key, $positions)) $position = $positions[$position_key];
+			
+			$npc_formatted_name = implode(" ", $npc_name_parts);
+			if (!isset($attendance[$npc_who])) {
+				$attendance[$npc_who] = array(
+					"name"			=>	$npc_who,
+					"display_name"	=>	"{$npc_formatted_name} ({$formatted_name})",
+					"position"		=>	$position,
+					"lines"			=>	0,
+				);
+			}
+			$who = $npc_who;
+			$formatted_name = "{$npc_formatted_name} ({$formatted_name})";
 
+			$normalized_message = jcommon_normalize_string($npc_match[2]);
+			$encoded_message    = htmlentities($normalized_message);
 
-        $attendance[$last_who]['lines']++;
+		}
 
-        $output .=  "\t<!-- {$timestamp} -->\n";
-        
-    
+		if ($last_who && strtolower($who) != $last_who) $output .= "</div>\n\n";
+		if (strtolower($who) != $last_who) {
+			$output .=  "<div id=\"line_{$key}\" class=\"chatline {$who}\">\n";
+			$output .=  "\t<div id=\"line_{$key}_who\" class=\"who {$position} {$who}\">{$formatted_name} says:</div>\n";
+			$last_who = strtolower($who);
+			if (!isset($attendance[$last_who])) {
+				$attendance[$last_who] = array(
+					"name"          =>  $who,
+					"display_name"  =>  $formatted_name,
+					"position"      =>  $position,
+					"lines"         =>  0,
+				);
+			}
+		}
+		$output .= "<!-- {$line} -->\n";		
 
-        if (preg_match("@<<<+([^>]+)>>>+@msi", $raw_message, $divider_message_match)) {
-            
-            $new_raw_message = jcommon_transliterate_string($divider_message_match[1]);
-            $encoded_message = htmlentities($new_raw_message);
-            if (preg_match("@[\"\']([^\'\"]+)[\"\']\s*\-*\s*Episode\s*([\dIVXLCDM]+)@msi", $new_raw_message, $begin_line_matches)) {
-                $arc_title = $begin_line_matches[1];
-                $episode_number = $begin_line_matches[2];
-            }
-            
-            $output .= "\t<p id=\"line_{$key}_divider_message\" class=\"divider_message\">{$encoded_message}</p>\n\n";
-        } else {
-            if (preg_match("@Episode Title: (.*)@msi", $raw_message, $episode_title_match)) {
-                $episode_title = $episode_title_match[1];
-            }
-            $output .=  "\t<p id=\"line_{$key}_message\" class=\"message\">{$encoded_message}</p>\n";
-        }
-        
+		$attendance[$last_who]['lines']++;	
 
-    }
+		if (preg_match("@<<<+([^>]+)>>>+@msi", $raw_message, $divider_message_match)) {
+			
+			$new_raw_message = jcommon_normalize_string(jcommon_transliterate_string($divider_message_match[1]));
+			$encoded_message = str_replace(array('<','>'),array('&lt;','&gt;'), $new_raw_message);
+
+			if (preg_match("@[\"\']([^\'\"]+)[\"\']\s*\-*\s*Episode\s*([\dIVXLCDM]+)@msi", $new_raw_message, $begin_line_matches)) {
+				$arc_title = $begin_line_matches[1];
+				$episode_number = $begin_line_matches[2];
+			}
+			
+			$output .= "\t<h4 id=\"line_{$key}_divider_message\" class=\"divider_message\">{$encoded_message}</h4>\n\n";
+		} else {
+			if (preg_match("@Episode Title: (.*)@msi", $raw_message, $episode_title_match)) {
+				$episode_title = $episode_title_match[1];
+			}
+			$output .=  "\t<p id=\"line_{$key}_message\" class=\"message\">{$encoded_message}</p>\n";
+		}
+		
+
+	} else {
+		$output .= "<!-- {$line} -->\n";
+		// if ($last_who) {
+		// 	$output .= "</div>\n\n";
+		// 	$last_who = "";
+		// }
+		// $new_raw_message = jcommon_normalize_string(jcommon_transliterate_string($line));
+		// $encoded_message = str_replace(array('<','>'),array('&lt;','&gt;'), $new_raw_message);
+		// $output .= "<div class=\"raw_transcript_line\">{$encoded_message}</div>\n";
+	}
 }
 $output .=  "</div>\n\n";
 
 $episode_description = array();
 
 if ($arc_title) {
-    $episode_description[] = "&ldquo;{$arc_title}&rdquo;";
-    if ($episode_number) {
-        $episode_description[] = "Episode {$episode_number}";
-    }
-    if ($episode_title) {
-        $episode_description[] = "&ldquo;{$episode_title}&rdquo;";
-    }
-    
+	$episode_description[] = "&ldquo;{$arc_title}&rdquo;";
+	if ($episode_number) {
+		$episode_description[] = "Episode {$episode_number}";
+	}
+	if ($episode_title) {
+		$episode_description[] = "&ldquo;{$episode_title}&rdquo;";
+	}
+	
 }
 
 echo $twig->render("index.twig", 
-    array(
-        "stardate"              =>  $date_display,
-        "attendance"            =>  $attendance,
-        "episode_description"   =>  $episode_description,
-        "transcript"            =>  $output,
-    )
-)
+	array(
+		"transcript_list"       =>  $transcript_list,
+		"stardate"              =>  $date_display,
+		"attendance"            =>  $attendance,
+		"episode_description"   =>  $episode_description,
+		"transcript"            =>  $output,
+	)
+);
 
 
 ?>
